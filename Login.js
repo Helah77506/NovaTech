25010a3e91aa1966ffa9c68abf8c9fa0212270d8
ali-15-a <240082563@aston.ac.uk>
2025-11-25 12:47:36 +0000

    methods added to login.js
    a few changes have also been made to login html to connect them up

--- Login.js
+++ Login.js
@@ -2,16 +2,39 @@
 
 //this function will listen for submission on the login page 
 function listen_Sumbission(){
+    const form = document.querySelector("form")
 
+    form.addEventListener("submit",function(e){
+        validateInputs(); 
+        e.preventDefault();    
+    })
 }
 
 //function to validate inputs
-function validateInputs(){
-    //ensure there are no empty fields 
+function validateInputs(){ 
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+    const label = document.getElementById("infolabel")
+    const pw = password.value.trim()
+    //ensure there are no empty fields
+    if(username.value.trim()  ==""||pw == ""){
+        //console.log("validate input test run ")
+        label.hidden = false
+        label.textContent = "Please ensure all fields are filled out"
+        return false
+    } 
+    //ensure password is more then 8 characters 
+    else if(pw.length<8||!/[a-z]/.test(pw)||!/[A-Z]/.test(pw)||!/[0-9]/.test(pw)){
+        label.hidden = false
+        label.textContent = "Please ensure the password meets the requirements\n-8 or more characters"+
+        " \n-at least one uppercase character \n-at least one lowercase character\n-at least one number "
+        return false
 
-    //ensure password meets requirments 
-
-    //check email is in correct format - if applicable 
+    }
+    else{
+       label.hidden = true
+       return true 
+    }   
 }
 
 //function which creates a login request and submits it to the backend 
@@ -38,3 +61,5 @@ function showPassword(){
 function showLoading(){
 
 }
+
+document.addEventListener("DOMContentLoaded", listen_Sumbission);
