1bffeb57a57f491bcb891244e5f88b0d3270f388
abdikhaliq1-web <240206262@aston.ac.uk>
2026-03-04 17:06:25 +0000

    Final review page updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'novatech/ecommerce-platform',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0b0712d823fd4438702ff02a2dca5dd21f37ccf8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'novatech/ecommerce-platform' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0b0712d823fd4438702ff02a2dca5dd21f37ccf8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'c7111310c6116ba508a6a170a89eaaed2129bd42',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
