05ed0ea400b04f166f228f600715ba2a4f61762a
ali-15-a <240082563@aston.ac.uk>
2026-03-14 14:00:39 +0000

    h

--- AuthenticationSec/loggedincheck.php
+++ AuthenticationSec/loggedincheck.php
@@ -1,3 +1,9 @@
 <?php 
 //this file will be used to validate if a user is logged in - based on what pages they can access 
+//if the user is not logged in 
+session_start();
+if(!isset($_SESSION['user_id'])){
+    header("Location: /Loginpage.php");
+    exit();
+}
 ?>
